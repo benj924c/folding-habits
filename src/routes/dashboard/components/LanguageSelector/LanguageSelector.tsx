10dd8ed163317d25012b9b56a82a7d160f529d55
Benjamin Vinterberg <benjaminvinterberg@gmail.com>
2023-12-08 16:09:56 +0100

    chore: show user languages and select one

--- src/routes/dashboard/components/LanguageSelector/LanguageSelector.tsx
+++ src/routes/dashboard/components/LanguageSelector/LanguageSelector.tsx
@@ -1,10 +1,17 @@
-import { component$, useContext } from "@builder.io/qwik"
+import { $, component$, useContext } from "@builder.io/qwik"
 import "@flags"
 import AddLanguageButton from "../AddLanguageButton"
-import { currentLanguageContext } from "../.."
+import { currentLanguageContext, useGetUserLanguages } from "../.."
 
 export const LanguageSelector = component$(() => {
   const currentLanguage = useContext(currentLanguageContext)
+  const userLanguagesData = useGetUserLanguages()
+  const handleClick = $(
+    ({ country, language }: { country: string; language: string }) => {
+      currentLanguage.country = country
+      currentLanguage.language = language
+    },
+  )
   // TODO: Map over user's languages and display them
   // TODO: Add a button to add a language (take the one you already made)
   // TODO: Add a button to remove a language (maybe a trash can icon on hover)
@@ -24,9 +31,25 @@ export const LanguageSelector = component$(() => {
           tabIndex={0}
           class="dropdown-content z-[1] shadow bg-base-100 rounded-box flex gap-2 p-2 align-middle"
         >
-          <button>
-            <span class="fi fi-gr fis text-5xl mask mask-circle" />
-          </button>
+          {userLanguagesData.value.data?.map((language) => {
+            if (language.country_code_name === currentLanguage.country)
+              return null
+            return (
+              <button
+                key={language.country_code_name}
+                onClick$={() =>
+                  handleClick({
+                    country: language.country_code_name,
+                    language: language.language,
+                  })
+                }
+              >
+                <span
+                  class={`fi fi-${language.country_code_name} fis text-5xl mask mask-circle`}
+                />
+              </button>
+            )
+          })}
           <AddLanguageButton />
         </div>
       </div>
