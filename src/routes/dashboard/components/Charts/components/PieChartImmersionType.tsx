df2af32fef5d8eca01da1b45fd00eb192e572da2
Benjamin Vinterberg <benj924c@stud.kea.dk>
2023-12-10 23:28:45 +0100

    chore: add disabling linting rule

--- src/routes/dashboard/components/Charts/components/PieChartImmersionType.tsx
+++ src/routes/dashboard/components/Charts/components/PieChartImmersionType.tsx
@@ -1,4 +1,11 @@
-import { component$, useContext, useSignal, useStore, useTask$, useVisibleTask$ } from "@builder.io/qwik"
+import {
+  component$,
+  useContext,
+  useSignal,
+  useStore,
+  useTask$,
+  useVisibleTask$,
+} from "@builder.io/qwik"
 import { Chart } from "chart.js/auto"
 import { immersionDataContext } from "~/routes/dashboard"
 
@@ -12,65 +19,80 @@ export const PieChartImmersionType = component$(() => {
     studyTotal: 0,
   })
   if (immersionData.data == null) {
-    return (
-      <div>Loading</div>
-    )
+    return <div>Loading</div>
   }
 
   useTask$(({ track }) => {
-    track(() => (immersionData.data))
+    track(() => immersionData.data)
     immersionTotal.activeTotal = 0
     immersionTotal.passiveTotal = 0
     immersionTotal.studyTotal = 0
 
     immersionData.data?.forEach((session) => {
-      if (session.immersion_type === 'active') {
-        immersionTotal.activeTotal += (session.seconds_immersed / 60)
+      if (session.immersion_type === "active") {
+        immersionTotal.activeTotal += session.seconds_immersed / 60
       }
-      if (session.immersion_type === 'passive') {
-        immersionTotal.passiveTotal += (session.seconds_immersed / 60)
+      if (session.immersion_type === "passive") {
+        immersionTotal.passiveTotal += session.seconds_immersed / 60
       }
-      if (session.immersion_type === 'study') {
-        immersionTotal.studyTotal += (session.seconds_immersed / 60)
+      if (session.immersion_type === "study") {
+        immersionTotal.studyTotal += session.seconds_immersed / 60
       }
     })
   })
 
   const mytheme = {
-    "primary": "#ff7ac6",
-    "secondary": "#bf95f9",
-    "accent": "#ffb86b",
-    "neutral": "#414558",
+    primary: "#ff7ac6",
+    secondary: "#bf95f9",
+    accent: "#ffb86b",
+    neutral: "#414558",
     "base-100": "#272935",
-    "info": "#8be8fd",
-    "success": "#52fa7c",
-    "warning": "#f1fa89",
-    "error": "#ff5757",
+    info: "#8be8fd",
+    success: "#52fa7c",
+    warning: "#f1fa89",
+    error: "#ff5757",
   }
 
+  // eslint-disable-next-line qwik/no-use-visible-task
   useVisibleTask$(({ track, cleanup }) => {
-    track(() => (immersionTotal.activeTotal, immersionTotal.passiveTotal, immersionTotal.studyTotal ))
+    track(
+      () => (
+        immersionTotal.activeTotal,
+        immersionTotal.passiveTotal,
+        immersionTotal.studyTotal
+      ),
+    )
     const pieChart = new Chart(canvasRef.value ?? "", {
       type: "doughnut",
       data: {
-        labels: ['Active Immersion', 'Passive Listening', 'Active Study'],
-        datasets: [{
-          label: 'Minutes Immersed',
-          data: [immersionTotal.activeTotal, immersionTotal.passiveTotal, immersionTotal.studyTotal],
-          backgroundColor: [mytheme["primary"], mytheme["secondary"], mytheme["accent"]],
-          borderColor: mytheme["neutral"],
-          borderWidth: 3
-        }]
+        labels: ["Active Immersion", "Passive Listening", "Active Study"],
+        datasets: [
+          {
+            label: "Minutes Immersed",
+            data: [
+              immersionTotal.activeTotal,
+              immersionTotal.passiveTotal,
+              immersionTotal.studyTotal,
+            ],
+            backgroundColor: [
+              mytheme["primary"],
+              mytheme["secondary"],
+              mytheme["accent"],
+            ],
+            borderColor: mytheme["neutral"],
+            borderWidth: 3,
+          },
+        ],
       },
     })
     pieChart.update()
     cleanup(() => pieChart.destroy())
   })
 
-  return(
+  return (
     <>
       <div class="w-[25rem] h-[25rem]">
-        <canvas class="w-full h-full" ref={canvasRef}/>
+        <canvas class="w-full h-full" ref={canvasRef} />
       </div>
     </>
   )
