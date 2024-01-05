f9502c2faf41276c37702d7b854b649d90c005ff
Benjamin Vinterberg <benjaminvinterberg@gmail.com>
2024-01-05 16:14:15 +0100

    chore: make responsive WIP

--- src/routes/components/footer.tsx
+++ src/routes/components/footer.tsx
@@ -4,15 +4,31 @@ import { Image } from "@unpic/qwik"
 export const Footer = component$(() => {
   return (
     <footer class="footer footer-center p-10 bg-base-200 text-base-content rounded w-full justify-self-end">
-      <div class="grid grid-flow-col gap-4">
-        <a href="/use-of-icons" class="link link-hover">Use of Icons</a>
-        <a href="/roadmap" class="link link-hover">Roadmap</a>
-        <a href="https://refold.la/" target="_blank" class="link link-hover">Refold</a>
-        <a href="/about" class="link link-hover">About</a>
+      <div class="flex flex-col sm:flex-row gap-4">
+        <a href="/use-of-icons" class="link link-hover">
+          Use of Icons
+        </a>
+        <a href="/roadmap" class="link link-hover">
+          Roadmap
+        </a>
+        <a href="https://refold.la/" target="_blank" class="link link-hover">
+          Refold
+        </a>
+        <a href="/about" class="link link-hover">
+          About
+        </a>
       </div>
       <div>
-        <a class="hover:scale-105 transition-all" href="https://ko-fi.com/juicybenjamin" target="_blank">
-          <Image width="223" height="30" src="https://ko-fi.com/img/githubbutton_sm.svg" />
+        <a
+          class="hover:scale-105 transition-all"
+          href="https://ko-fi.com/juicybenjamin"
+          target="_blank"
+        >
+          <Image
+            width="223"
+            height="30"
+            src="https://ko-fi.com/img/githubbutton_sm.svg"
+          />
         </a>
       </div>
       <div>
@@ -21,4 +37,5 @@ export const Footer = component$(() => {
         </p>
       </div>
     </footer>
-  )})
+  )
+})
